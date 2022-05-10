991999f94bbdbe40ba1e3786bed4bd470afa9951
kotuvijay <104496284+kotuvijay@users.noreply.github.com>
2022-05-10 17:04:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<body style="background-color:yellow;">
+<body style="background-color:lightpink;">
 <font size = "6"><p style="color:red">This is Stagging deployment Slot.</p></font>
 
 <?php
