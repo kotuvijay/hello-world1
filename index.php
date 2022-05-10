1e31e78c6008827f403a3f17416fdbd604e5629c
kotuvijay <104496284+kotuvijay@users.noreply.github.com>
2022-05-10 16:20:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<body style="background-color:powderblue;">
+<font size = "6"><p style="color:red">This is Stagging deployment Slot.</p></font>
+
+<?php
+echo "Azure App Service Example";
+?> 
+
+</body>
+</html>
